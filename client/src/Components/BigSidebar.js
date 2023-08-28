d60d76b8831984ca56e7493a0f05386e3c35da2d
Ayush Petkar <73267898+Ayush456-P@users.noreply.github.com>
2023-08-28 14:42:06 +0530

    Footer Fixed

--- client/src/Components/BigSidebar.js
+++ client/src/Components/BigSidebar.js
@@ -5,7 +5,7 @@ import Logo from './Logo'
 
 const BigSidebar = () => {
     const { showSidebar } = useAppContext()
-   
+    const year = new Date().getFullYear()
     return (
         <Wrapper>
             <div className={showSidebar ? 'sidebar-container' : 'sidebar-container show-sidebar'}>
@@ -15,8 +15,11 @@ const BigSidebar = () => {
                     </header>
                     <NavLinks />
                 </div>
-                
-                
+                <footer style={{
+                    position: 'fixed', bottom: 0, width: '100%', height: '60px', paddingLeft:'40px'
+                }}>
+                <p>&copy; {year} Job Hunt</p>
+                </footer>
             </div>
         </Wrapper >
     )
